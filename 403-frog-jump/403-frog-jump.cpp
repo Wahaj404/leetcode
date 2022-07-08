b1aaedff41fe0b89cf895d7dc24f4004427d5e71
Wahaj404 <57715039+Wahaj404@users.noreply.github.com>
2022-07-08 18:51:11 +0500

    Time: 185 ms (69.78%), Space: 51.4 MB (44.91%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -4,16 +4,15 @@ public:
         if (stones[1] != 1) return false;
         auto last = stones.back();
         stones.pop_back();
-        unordered_map<long, unordered_set<long>> v;
+        unordered_map<int, unordered_set<int>> v;
         v[1] = {1};
-        for (long cur : stones) {
-            for (long x : v[cur]) {
-                long next = cur + x;
+        for (auto cur : stones) {
+            for (auto x : v[cur]) {
+                auto next = cur + x;
                 v[next - 1].insert(x - 1);  
                 v[next].insert(x);
                 v[next + 1].insert(x + 1);
             }
-            v.erase(cur);
         }
         return v.count(last);
     }
