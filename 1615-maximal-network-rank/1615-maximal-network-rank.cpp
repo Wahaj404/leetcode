a6b2acd10e2a340be4628371bcb3accaee2609f5
Wahaj404 <57715039+Wahaj404@users.noreply.github.com>
2022-07-17 17:18:02 +0500

    Time: 208 ms (25.09%), Space: 39 MB (35.62%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<unordered_set<int>> v(n);
+        for (auto const& i : roads) {
+            v[i[0]].insert(i[1]);
+            v[i[1]].insert(i[0]);
+        }
+        int mx = INT_MIN;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i != j) {
+                    mx = max(mx, (int)(v[i].size() + v[j].size() - v[i].count(j)));             
+                }
+            }
+        }
+        return mx;
+    }
+};
